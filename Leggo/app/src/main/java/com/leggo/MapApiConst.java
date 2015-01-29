caba9d2581c4d97405f4a2ae7ac3205ae1b1871f
jsul9305 <jsul9305@gmail.com>
2015-01-29 09:21:53 +0900

    security api key and add README.md

--- Leggo/app/src/main/java/com/leggo/MapApiConst.java
+++ Leggo/app/src/main/java/com/leggo/MapApiConst.java
@@ -4,5 +4,5 @@ package com.leggo;
  * Created by user on 2015-01-28.
  */
 public class MapApiConst {
-    public static final String DAUM_MAPS_ANDROID_APP_API_KEY = "1ae2db626439a1e4ced2f402eb879ae5";
+    public static final String DAUM_MAPS_ANDROID_APP_API_KEY = "DAUM_API_KEY";
 }
